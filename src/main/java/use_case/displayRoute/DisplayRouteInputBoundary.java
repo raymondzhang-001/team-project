3d69032b4a584321ddf23cb9e1a90933257ead83
raymondzhang-001 <ramon.zhang@mail.utoronto.ca>
2025-11-16 02:54:08 +0800

    Implemented DisplayRouteInputBoundary

--- src/main/java/use_case/displayRoute/DisplayRouteInputBoundary.java
+++ src/main/java/use_case/displayRoute/DisplayRouteInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.displayRoute;
 
+/**
+ * Input Boundary for actions related to calculating and displaying a route.
+ */
+
 public interface DisplayRouteInputBoundary {
+    /**
+     * Executes the Display Route use case. *
+     * @param inputData the input data containing start and end coordinates
+     */
+    void execute(DisplayRouteInputData inputData);
 }
