c0860b13ba1a17ffb4ec031c56cbff20dc999a01
raymondzhang-001 <ramon.zhang@mail.utoronto.ca>
2025-11-16 02:48:31 +0800

    Implemented DisplayRouteInputData, need to change after finish implementing marker entity

--- src/main/java/use_case/displayRoute/DisplayRouteInputData.java
+++ src/main/java/use_case/displayRoute/DisplayRouteInputData.java
@@ -1,4 +1,32 @@
 package use_case.displayRoute;
 
 public class DisplayRouteInputData {
+
+    private final double startLatitude;
+    private final double startLongitude;
+    private final double endLatitude;
+    private final double endLongitude;
+
+    public DisplayRouteInputData(double startLatitude, double startLongitude, double endLatitude, double endLongitude) {
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+    }
+
+    public double getStartLatitude() {
+        return startLatitude;
+    }
+
+    public double getStartLongitude() {
+        return startLongitude;
+    }
+
+    public double getEndLatitude() {
+        return endLatitude;
+    }
+
+    public double getEndLongitude() {
+        return endLongitude;
+    }
 }
