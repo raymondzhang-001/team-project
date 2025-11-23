839019b90ad9ae57d391b47d0a78968ebd1c38dd
unknown <white040707@gmail.com>
2025-11-22 20:13:32 -0500

    .

---
+++ src/main/java/use_case/add_marker/AddMarkerInputBoundary.java
@@ -0,0 +1,10 @@
+package use_case.add_marker;
+
+public interface AddMarkerInputBoundary {
+    /**
+     *  executes the AddMarker use case
+     * @param inputData the input data
+     */
+    void execute(AddMarkerInputData inputData);
+
+}
